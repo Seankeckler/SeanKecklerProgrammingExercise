f41065baf9a2965837407463d0f2cd412e20b162
Sean Keckler <seankeckler@gmail.com>
2019-02-03 12:34:53 -0500

    Add project files.

---
+++ BravoLtCodeExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BravoLtCodeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
